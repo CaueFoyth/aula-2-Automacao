24acefd69af6f40788303ba10bbad12e02c7a5ad
CaueFoyth <foythcaue@gmail.com>
2023-07-25 19:57:59 -0300

    A

---
+++ Gabarito/codigo.py
@@ -0,0 +1,22 @@
+# Passo a passo do projeto
+# Passo 1: Entrar no sistema da empresa
+
+# Entrar no link https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# pip install pyautogui
+
+import pyautogui
+
+# pyautogui.write -> Escrever um texto
+# pyautogui.press -> Apertar uma tecla
+# pyautogui.click -> Clicar em algum lugar
+
+# Abrir navegador
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+
+# Passo 2: Fazer login
+# Passo 3: Importar a base de produtos
+# Passo 4: Cadastrar o produto
+# Passo 5: Cadastrar até acabar os produtos
